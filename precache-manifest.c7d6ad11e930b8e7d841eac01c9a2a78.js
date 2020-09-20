eedfbd0d910b57a10f7baedef7982b4567d58cf1
muyildirim <musyil@netrd.com.tr>
2020-09-20 03:37:19 +0300

    Updates

---
+++ precache-manifest.c7d6ad11e930b8e7d841eac01c9a2a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c23d1dcf2f11e15ef528c454b894716",
+    "url": "/personal-website-react/index.html"
+  },
+  {
+    "revision": "761eb00f6e3922ea848c",
+    "url": "/personal-website-react/static/css/main.e75b8e03.chunk.css"
+  },
+  {
+    "revision": "15b4e89bba848f3aa832",
+    "url": "/personal-website-react/static/js/2.f00508e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website-react/static/js/2.f00508e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761eb00f6e3922ea848c",
+    "url": "/personal-website-react/static/js/main.dd79d636.chunk.js"
+  },
+  {
+    "revision": "cd108fa24e6d006a4c4e",
+    "url": "/personal-website-react/static/js/runtime-main.b56417b6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/personal-website-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.0c080522e89311a5cfb0809b834fd881.js"
+  "/personal-website-react/precache-manifest.c7d6ad11e930b8e7d841eac01c9a2a78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
