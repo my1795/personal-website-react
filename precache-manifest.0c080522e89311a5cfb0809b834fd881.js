081fed1a020365d2135036edf4eebca0bc750cd2
muyildirim <musyil@netrd.com.tr>
2020-09-20 03:32:15 +0300

    Updates

---
+++ precache-manifest.0c080522e89311a5cfb0809b834fd881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e82c0be14ecb64ce7f3da17364202a",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "532f2a5ccd43bdbb4e57",
+    "url": "/me/static/css/main.e75b8e03.chunk.css"
+  },
+  {
+    "revision": "15b4e89bba848f3aa832",
+    "url": "/me/static/js/2.f00508e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.f00508e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532f2a5ccd43bdbb4e57",
+    "url": "/me/static/js/main.e91ea9aa.chunk.js"
+  },
+  {
+    "revision": "da3ee6f0dcd23f139ed7",
+    "url": "/me/static/js/runtime-main.490eddd9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/me/static/media/logo.ee7cd8ed.svg"
+  }
+]);
